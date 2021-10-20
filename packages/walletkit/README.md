c0f94ef1d6eb184f8fe4916980bc3f94361d0f57
Goki Rajesh <89615631+sungoki@users.noreply.github.com>
2021-10-20 10:00:45 -0500

    Migrate to Yarn 3; update dependencies (#98)
    * Migrate to Yarn 3; dependency updates
    
    * Fix yarn doctor output
    
    * Run yarn doctor in CI
    
    * eslint pin
    
    * remove unused dep
    
    * Update cache keys, fix publish
    
    * prettier yaml
    
    * Clean up dependencies
    
    * Delete redundant core dep
    
    * update readme and add classname for modal wrapper
    
    * Fix yarn doctor

--- packages/walletkit/README.md
+++ packages/walletkit/README.md
@@ -1,3 +1,15 @@
 # `@gokiprotocol/walletkit`
 
 WalletKit is a React library that allows a Solana dApp to display a modal for connecting wallets.
+
+## Common questions
+
+### My modal is behind something else. How do I fix this?
+
+You can change the z-index of the modal like so:
+
+```
+.goki-walletkit-modal-wrapper {
+    z-index: 9999999;
+}
+```
--- packages/walletkit/src/components/Modal/index.tsx
+++ packages/walletkit/src/components/Modal/index.tsx
@@ -1,10 +1,10 @@
 import { css, Global } from "@emotion/react";
 import styled from "@emotion/styled";
 import { DialogContent, DialogOverlay } from "@reach/dialog";
+import { animated, useSpring, useTransition } from "@react-spring/web";
 import darken from "polished/lib/color/darken";
 import React from "react";
 import { isMobile } from "react-device-detect";
-import { animated, useSpring, useTransition } from "react-spring";
 import { useGesture } from "react-use-gesture";
 
 import { BackIcon, CloseIcon, SolanaLogo } from "./icons";
@@ -93,6 +93,7 @@ export const Modal: React.FC<ModalProps> = ({
               darkenOverlay={darkenOverlay}
             >
               <ModalWrapper
+                className="goki-walletkit-modal-wrapper"
                 aria-label="dialog content"
                 {...(isMobile
                   ? {
