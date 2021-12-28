b470ffd813097e2d37df9007b97ec3e801f5463c
Goki Rajesh <goki@goki.so>
2021-12-29 02:22:55 +0530

    Fix build

--- packages/example/craco.config.js
+++ packages/example/craco.config.js
@@ -1,3 +1,5 @@
+const webpack = require("webpack");
+
 module.exports = {
   babel: {
     presets: [
@@ -12,4 +14,31 @@ module.exports = {
     enable: false,
   },
   typescript: { enableTypeChecking: false },
+  webpack: {
+    configure: (config) => {
+      config.plugins.unshift(
+        new webpack.ProvidePlugin({
+          Buffer: ["buffer", "Buffer"],
+        })
+      );
+
+      config.module.rules.push({
+        test: /\.m?js/,
+        resolve: {
+          fullySpecified: false,
+        },
+      });
+
+      // solana wallet adapter, ledger need to be transpiled
+      config.module.rules.push({
+        test: /\.js/,
+        loader: require.resolve("babel-loader"),
+        exclude: (file) =>
+          !file.includes("@solana/wallet-adapter") &&
+          !file.includes("@ledgerhq/devices") &&
+          !file.includes("@saberhq/use-solana"),
+      });
+      return config;
+    },
+  },
 };
